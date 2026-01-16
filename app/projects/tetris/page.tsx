96fcd5c75c5536e4ca0923c3e201a5d50e11d394
Benjamin Kiel <benjamin.kiel@vorwerk.de>
2026-01-16 15:40:34 +0100

    Add Tetris page without content

---
+++ app/projects/tetris/page.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { ParallaxScrollGallery, ParallaxItemType } from "@/components/ui/parallax-scroll";
+import {
+  IconBox,
+  IconBrain,
+  IconBrush,
+  IconDeviceGamepad2,
+  IconEye,
+  IconRoute,
+} from "@tabler/icons-react";
+
+export default function Dashboard() {
+  return (
+    <div className="w-full h-full bg-neutral-950"></div>
+  );
+}
+
